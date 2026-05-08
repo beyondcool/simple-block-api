be2c126f6d0ed6e87bfa9e0ed407ece7eae82b62
zhouhe <109071103@qq.com>
2026-05-09 07:33:21 +0800

    add log

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,9 +1,13 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func MyLogger(c *gin.Context) {
-
+	fmt.Printf("Request 【%s】start...\n", c.Request.URL.Path)
 	c.Next()
-
+	fmt.Printf("Request 【%s】end\n\n", c.Request.URL.Path)
 }
